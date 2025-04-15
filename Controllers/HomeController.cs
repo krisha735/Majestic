8af8989e15408ebd6586a89280c0e0c81f070c9d
krisha735 <krishanandakumar735@gmail.com>
2025-04-15 21:58:07 +0800

    Dashboard Admin

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Majestic.Models;
+using Majestic.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Majestic.Controllers
@@ -15,7 +16,26 @@ namespace Majestic.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // Mock data for destinations
+            var destinations = new List<Destination>
+            {
+                new Destination { Id = 1, Name = "Bali", Description = "Island of the Gods", Package = "5D4N", ImageUrl = "~/Images/bali.jpg" },
+                new Destination { Id = 2, Name = "Paris", Description = "City of Lights", Package = "7D6N", ImageUrl = "~/Images/paris.jpg" },
+                new Destination { Id = 3, Name = "Langkawi", Description = "Beautiful islands", Package = "3D2N", ImageUrl = "~/Images/langkawi.jpg" }
+            };
+
+            // Check if the user is admin (you can adjust this to a more robust check later)
+            bool isAdmin = User.Identity.Name == "admin@majestic.com";
+
+            // Create the ViewModel
+            var viewModel = new DestinationListViewModel
+            {
+                Destinations = destinations,
+                IsAdmin = isAdmin
+            };
+
+            // Pass the viewModel to the View
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
