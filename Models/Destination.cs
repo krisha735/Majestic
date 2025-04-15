cb65859e82dc61270b3fe785ad43d16f36325dc8
krisha735 <krishanandakumar735@gmail.com>
2025-04-15 22:03:19 +0800

    Add files via upload

---
+++ Models/Destination.cs
@@ -0,0 +1,13 @@
+﻿using Majestic.Models;
+
+namespace Majestic.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Package { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
