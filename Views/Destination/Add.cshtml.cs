44549528f376e8c3b88ed4f63f84357cd80666e2
krisha735 <krishanandakumar735@gmail.com>
2025-04-15 22:09:38 +0800

    Add files via upload

---
+++ Views/Destination/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Majestic.Views.Destination
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
